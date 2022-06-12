80fef0775846f6e007cc7d447449c7b09d8b86d3
Lockheed-stack <986667243@qq.com>
2022-06-12 15:33:59 +0800

    2022/06/12

--- social_network_hw/implemented_algorithms/InfluenceBasedLocalRandomWalk.py
+++ social_network_hw/implemented_algorithms/InfluenceBasedLocalRandomWalk.py
@@ -1,9 +1,9 @@
 #%%
 import networkx as nx
-import time
-import cupy as cp
+import torch
 import scipy.stats
-import numpy as np
+#import numpy as np
+from tqdm import tqdm
 #%%
 def create_transition_probability_matrix(G,num_of_node,node_influence_power):
     '''return a transition probability matrix.
@@ -11,18 +11,19 @@ def create_transition_probability_matrix(G,num_of_node,node_influence_power):
     because there is no "node 0" or may miss some nodes
     in the dataset of "scholat link prediction".
     '''
-    t_p_m = np.zeros((num_of_node+1,num_of_node+1))
+    dev = torch.device('cuda')
+    t_p_m = torch.zeros((num_of_node+1,num_of_node+1),device=dev)
     
     for node_x in nx.nodes(G):
         sum_of_influence = 0
         for nbr in nx.neighbors(G,node_x):
-            sum_of_influence=np.add(sum_of_influence,node_influence_power[nbr])
+            sum_of_influence+=node_influence_power[nbr]
         
         for nbr in nx.neighbors(G,node_x): 
             if sum_of_influence==0:
                 t_p_m[node_x][nbr]=0
             else:
-                t_p_m[node_x][nbr]=np.divide(node_influence_power[nbr],sum_of_influence)
+                t_p_m[node_x][nbr]=node_influence_power[nbr]/sum_of_influence
             
     return t_p_m
 #%%
@@ -30,11 +31,12 @@ def local_random_walk(time_steps,start_node,num_of_nodes,trans_prob_matrix):
     '''
     return a probability list after t steps
     '''
-    t_step_output = cp.zeros((1,num_of_nodes+1))
+    dev = torch.device('cuda')
+    t_step_output = torch.zeros((1,num_of_nodes+1),device=dev)
     t_step_output[0][start_node]=1
     
     for steps in range(time_steps):
-        t_step_output=cp.dot(t_step_output,trans_prob_matrix)
+        torch.mm(t_step_output,trans_prob_matrix,out=t_step_output)
 
         
     return t_step_output
@@ -55,7 +57,7 @@ def nbr_influence_power(G):
     
     return node_influence_power
 #%%
-def score_LRW(G,time_steps,start_node):
+def score_ILRW(G,time_steps,start_node):
     '''
     return a sorted dictionary of potential link for start node
     '''
@@ -64,57 +66,59 @@ def score_LRW(G,time_steps,start_node):
     score_dict={}
     nbr_influ = nbr_influence_power(G)
     trans_prob_matrix = create_transition_probability_matrix(G,num_of_nodes,nbr_influ)
-    cp_trans_prob_matrix = cp.asarray(trans_prob_matrix)
-    t_steps_output = local_random_walk(time_steps,start_node,num_of_nodes,cp_trans_prob_matrix)
+    #cp_trans_prob_matrix = cp.asarray(trans_prob_matrix)
+    t_steps_output = local_random_walk(time_steps,start_node,num_of_nodes,trans_prob_matrix)
     
-    curr_progress = 0
-    start_time = time.perf_counter()
+    #curr_progress = 0
+    #start_time = time.perf_counter()
     
-    for node in nx.nodes(G):
-        curr_progress+=1
-        
-        
-        if node != start_node:
-            if node not in nx.neighbors(G,start_node):
-                start_node_deg = nx.degree(G,start_node)
-                start_node_deg_div_2E = cp.divide(start_node_deg,double_E)
-                startNode_to_node =cp.multiply(start_node_deg_div_2E,t_steps_output[0][node])
-                
-                tempOutput = local_random_walk(time_steps,node,num_of_nodes,cp_trans_prob_matrix)
-                node_start_deg = nx.degree(G,node)
-                node_start_deg_div_2E = cp.divide(node_start_deg,double_E)
-                node_to_startNode =cp.multiply(node_start_deg_div_2E,tempOutput[0][start_node])
-                
-                score_dict[node]=cp.add(startNode_to_node,node_to_startNode)
-        
-        progress_bar(curr_progress,len(nx.nodes(G)),start_time)
+    with tqdm(total=len(nx.nodes(G))) as qbar:
+        for node in nx.nodes(G):
+            #curr_progress+=1
+            
+            
+            if node != start_node:
+                if node not in nx.neighbors(G,start_node):
+                    start_node_deg = nx.degree(G,start_node)
+                    start_node_deg_div_2E = start_node_deg/double_E
+                    startNode_to_node =start_node_deg_div_2E*t_steps_output[0][node]
+                    
+                    tempOutput = local_random_walk(time_steps,node,num_of_nodes,trans_prob_matrix)
+                    node_start_deg = nx.degree(G,node)
+                    node_start_deg_div_2E = node_start_deg/double_E
+                    node_to_startNode = node_start_deg_div_2E*tempOutput[0][start_node]
+                    
+                    score_dict[node]=startNode_to_node+node_to_startNode
+            
+            #progress_bar(curr_progress,len(nx.nodes(G)),start_time)
+            qbar.update(1)
    
     sorted_score_dict = sorted(score_dict.items(),key=lambda kv:kv[1],reverse=True)
     
     return sorted_score_dict
 #%%
-def progress_bar(cur_progress, total_progress, start_time):
-    scale = 50  # 进度条长度
-    cur_progress += 1  # 读出的excel表第一行为表头，第二行从0开始计数
-    time_now = time.perf_counter()
-    rate = total_progress/scale  # 总进度太长或太短，需要缩短或放大
-    if rate == 0:  # 防止出错
-        print('{:.0%}[{}->{}]{:.2f}s'.format(1, 50, 0, (time_now-start_time)))
-        return
+# def progress_bar(cur_progress, total_progress, start_time):
+#     scale = 50  # 进度条长度
+#     cur_progress += 1  # 读出的excel表第一行为表头，第二行从0开始计数
+#     time_now = time.perf_counter()
+#     rate = total_progress/scale  # 总进度太长或太短，需要缩短或放大
+#     if rate == 0:  # 防止出错
+#         print('{:.0%}[{}->{}]{:.2f}s'.format(1, 50, 0, (time_now-start_time)))
+#         return
 
-    completed = '*'*(int(cur_progress/rate))
-    uncompleted = '-'*int((total_progress-cur_progress)/rate)
-    percent = (cur_progress/total_progress)
-    if percent >= 1:
-        percent = 1
-    print('\r{:^.0%}[{}->{}]{:.2f}s'.format(percent,
-                                            completed, uncompleted, (time_now-start_time)), end='')
-    # if (cur_progress) >= total_progress:
-    #     print('')
+#     completed = '*'*(int(cur_progress/rate))
+#     uncompleted = '-'*int((total_progress-cur_progress)/rate)
+#     percent = (cur_progress/total_progress)
+#     if percent >= 1:
+#         percent = 1
+#     print('\r{:^.0%}[{}->{}]{:.2f}s'.format(percent,
+#                                             completed, uncompleted, (time_now-start_time)), end='')
+#     if (cur_progress) >= total_progress:
+#         print(' ,Done')
 
-# %%
-if __name__=='__main__':
-    G = nx.read_adjlist('./SCHOLAT Link Prediction/train.csv',nodetype=int,delimiter=',')
-    output = score_LRW(G,5,8777)
 #%%
-output
+# if __name__=='__main__':
+#     G = nx.read_adjlist('../SCHOLAT Link Prediction/train.csv',nodetype=int,delimiter=',')
+#     output = score_ILRW(G,5,8777)
+#     output
+# %%
--- social_network_hw/implemented_algorithms/LocalRandomWalk.py
+++ social_network_hw/implemented_algorithms/LocalRandomWalk.py
@@ -1,10 +1,8 @@
 #%%
-import time
-import numpy as np
+import torch
+#import numpy as np
 import networkx as nx
-import cupy as cp
-# from line_profiler import LineProfiler
-# import matplotlib.pyplot as plt
+from tqdm import tqdm
 # %%
 def create_transition_probability_matrix(G):
     '''return a transition probability matrix.
@@ -12,8 +10,9 @@ def create_transition_probability_matrix(G):
     because there is no "node 0" or may miss some nodes
     in the dataset of "scholat link prediction".
     '''
+    dev = torch.device('cuda')
     size_num = max(nx.nodes(G))
-    t_p_m = cp.zeros((size_num+1,size_num+1))
+    t_p_m = torch.zeros((size_num+1,size_num+1),device=dev)
     
     for node_x in nx.nodes(G):
         for nbr in nx.neighbors(G,node_x):
@@ -28,12 +27,13 @@ def local_random_walk(time_steps,start_node,num_of_nodes,trans_prob_matrix):
     return a probability list after t steps
     '''
     #final_output_dict={}
-    t_step_output = cp.zeros(num_of_nodes+1)
-    t_step_output[start_node]=1
+    dev = torch.device('cuda')
+    t_step_output = torch.zeros((1,num_of_nodes+1),device=dev)
+    t_step_output[0][start_node]=1
     
     for steps in range(time_steps):
-        t_step_output=cp.dot(t_step_output,trans_prob_matrix)
-        
+        #t_step_output=cp.dot(t_step_output,trans_prob_matrix)
+        torch.mm(t_step_output,trans_prob_matrix,out=t_step_output)
     # for i in range(1,max(nx.nodes(G))+1):
     #     final_output_dict[i]=t_step_output[i]
         
@@ -49,49 +49,80 @@ def score_LRW(G,time_steps,start_node):
     score_dict={}
     trans_prob_matrix = create_transition_probability_matrix(G)
     t_steps_output = local_random_walk(time_steps,start_node,num_of_nodes,trans_prob_matrix)
-    curr_progress = 0
-    start_time = time.perf_counter()
-    
-    for node in nx.nodes(G):
-        curr_progress+=1
-        progress_bar(curr_progress,len(nx.nodes(G)),start_time)
-        
-        if node != start_node:
-            if node not in nx.neighbors(G,start_node):
-                startNode_to_node = nx.degree(G,start_node)/(double_E)*t_steps_output[node]
-                
-                tempOutput = local_random_walk(time_steps,node,num_of_nodes,trans_prob_matrix)
-                node_to_startNode = nx.degree(G,node)/(double_E)*tempOutput[start_node]
-                
-                score_dict[node]=startNode_to_node+node_to_startNode
-    
+    # curr_progress = 0
+    # start_time = time.perf_counter()
+    with tqdm(total=len(nx.nodes(G))) as qbar:
+        for node in nx.nodes(G):
+            # curr_progress+=1
+            # progress_bar(curr_progress,len(nx.nodes(G)),start_time)
+            
+            if node != start_node:
+                if node not in nx.neighbors(G,start_node):
+                    startNode_to_node = nx.degree(G,start_node)/(double_E)*t_steps_output[0][node]
+                    
+                    tempOutput = local_random_walk(time_steps,node,num_of_nodes,trans_prob_matrix)
+                    node_to_startNode = nx.degree(G,node)/(double_E)*tempOutput[0][start_node]
+                    
+                    score_dict[node]=startNode_to_node+node_to_startNode
+            qbar.update(1)
     sorted_score_dict = sorted(score_dict.items(),key=lambda kv:kv[1],reverse=True)
     
     return sorted_score_dict    
 # %%
 # %%
-def progress_bar(cur_progress, total_progress, start_time):
-    scale = 50  # 进度条长度
-    cur_progress += 1  # 读出的excel表第一行为表头，第二行从0开始计数
-    time_now = time.perf_counter()
-    rate = total_progress/scale  # 总进度太长或太短，需要缩短或放大
-    # if rate == 0:  # 防止出错
-    #     print('{:.0%}[{}->{}]{:.2f}s'.format(1, 50, 0, (time_now-start_time)))
-    #     return
+# def progress_bar(cur_progress, total_progress, start_time):
+#     scale = 50  # 进度条长度
+#     cur_progress += 1  # 读出的excel表第一行为表头，第二行从0开始计数
+#     time_now = time.perf_counter()
+#     rate = total_progress/scale  # 总进度太长或太短，需要缩短或放大
+#     # if rate == 0:  # 防止出错
+#     #     print('{:.0%}[{}->{}]{:.2f}s'.format(1, 50, 0, (time_now-start_time)))
+#     #     return
 
-    completed = '*'*(int(cur_progress/rate))
-    uncompleted = '-'*int((total_progress-cur_progress)/rate)
-    percent = (cur_progress/total_progress)
-    if percent >= 1:
-        percent = 1
-    print('\r{:^.0%}[{}->{}]{:.2f}s'.format(percent,
-                                            completed, uncompleted, (time_now-start_time)), end='')
-    # if (cur_progress) >= total_progress:
-    #     print('')
-# %%
-if __name__=='__main__':
-    G = nx.read_adjlist('../SCHOLAT Link Prediction/train.csv',nodetype=int,delimiter=',')
-    output=score_LRW(G,6,8777)
-# %%
-output
+#     completed = '*'*(int(cur_progress/rate))
+#     uncompleted = '-'*int((total_progress-cur_progress)/rate)
+#     percent = (cur_progress/total_progress)
+#     if percent >= 1:
+#         percent = 1
+#     print('\r{:^.0%}[{}->{}]{:.2f}s'.format(percent,
+#                                             completed, uncompleted, (time_now-start_time)), end='',flush=True)
+#     if (cur_progress) >= total_progress:
+#         print(' ,Done')
+# # %%
+# if __name__=='__main__':
+#     G = nx.read_adjlist('../SCHOLAT Link Prediction/train.csv',nodetype=int,delimiter=',')
+#     output=score_LRW(G,6,8777)
+# # %%
+# output
+# # %%
+# def AUC(output,G_test,node):
+#     '''
+#     @return auc score
+#     @param:
+#         @output: list-like, list[set(),set(),..,set()]
+#         @G_test: the graph for test
+#         @node: start node
+#     '''
+#     missing_edge={}
+#     for i in output:
+#         if i[0] in nx.neighbors(G_test,node):
+#             missing_edge[i[0]]=i[1]
+    
+#     n1,n2 = 0,0
+#     for edge in missing_edge.items():
+#         for i in output:
+#             if i[0] not in nx.neighbors(G_test,node):#non-existent edge
+#                 if edge[1]<i[1]:
+#                     continue
+#                 elif edge[1]>i[1]:
+#                     n1+=1
+#                 else:
+#                     n2+=1
+#     n = len(missing_edge)*len(output)
+#     auc = (n1+0.5*n2)/n
+#     print(f'node {node}\'s AUC: {auc}')
+#     return auc
+# # %%
+# G_test = nx.read_adjlist('../SCHOLAT Link Prediction/test.csv',nodetype=int,delimiter=',')
+# AUC(output,G_test,8777)
 # %%
