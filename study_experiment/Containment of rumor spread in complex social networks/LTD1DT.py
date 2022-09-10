0ddb1925c967c69d06f2284bbc6946285d04c593
Lockheed-stack <986667243@qq.com>
2022-09-10 22:01:42 +0800

    问题很多, 还要改

--- study_experiment/Containment of rumor spread in complex social networks/LTD1DT.py
+++ study_experiment/Containment of rumor spread in complex social networks/LTD1DT.py
@@ -5,20 +5,28 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 #%%
 class Graph_with_attr:
-    def __init__(self,G:nx.Graph) -> None:
+    def __init__(self,G:nx.Graph,final_R_receiver:dict=None,final_T_receiver:dict=None,) -> None:
         self.G = nx.Graph(G)
-        self.final_R_receiver = {}
-        self.final_T_receiver = {}
+
+        if final_R_receiver is not None:
+            self.final_R_receiver = final_R_receiver.copy()
+        else:
+            self.final_R_receiver = {}
+
+        if final_T_receiver is not None:
+            self.final_T_receiver = final_T_receiver.copy()
+        else:
+            self.final_T_receiver = {}
     
     def copy(self,):
         G = nx.Graph(self.G)
-        return self.__class__(G)
+        return self.__class__(G,self.final_R_receiver,self.final_T_receiver)
 
     def get_final_R_receiver(self):
-        return self.final_R_receiver
+        return self.final_R_receiver.copy()
 
     def get_final_T_receiver(self):
-        return self.final_T_receiver
+        return self.final_T_receiver.copy()
 #%%
 class LTD1DT_model:
     def __init__(self,GWA:Graph_with_attr,seed_R:list,seed_T:list,keep_original_data:bool=False) -> None:
@@ -57,10 +65,13 @@ class LTD1DT_model:
         G = self.GWA.copy()
         return self.__class__(G,self.seed_R,self.seed_T)
 
-    def diffusion_simulation(self,specified_G:Graph_with_attr=None):
+    def diffusion_simulation(self,specified_G:Graph_with_attr=None,copy:bool=False):
         nothing_update = False
         time_steps = 0
         influenced_nodes = {}
+        
+        
+        #if copy is False:
 
         if specified_G is not None:
             G = specified_G.G
@@ -79,7 +90,10 @@ class LTD1DT_model:
                         influenced_nodes[node]=1
             
             # decision stage
-            search_range = list(influenced_nodes.keys())+list(specified_G.get_final_R_receiver().keys())
+            if specified_G is not None:
+                search_range = list(influenced_nodes.keys())+list(specified_G.get_final_R_receiver().keys())
+            else:
+                search_range = list(influenced_nodes.keys())+list(self.GWA.get_final_R_receiver().keys())
 
             for node in search_range:
                 if node  not in self.seed_R:
@@ -95,13 +109,17 @@ class LTD1DT_model:
 
         influenced_num = 0      
         node_deg = nx.degree(specified_G,node)
+        if specified_G is not None:
+            G = specified_G
+        else:
+            G = self.GWA.G
 
-        for nbr in nx.neighbors(specified_G,node):
-            if (specified_G.nodes[nbr]['status'] == 'R-active') or (specified_G.nodes[nbr]['status'] == 'T-active') :
+        for nbr in nx.neighbors(G,node):
+            if (G.nodes[nbr]['status'] == 'R-active') or (G.nodes[nbr]['status'] == 'T-active') :
                 influenced_num+=1
         
-        if (influenced_num/node_deg) >= specified_G.nodes[node]['i_threshold']:
-            specified_G.nodes[node]['status'] = 'influenced'
+        if (influenced_num/node_deg) >= G.nodes[node]['i_threshold']:
+            G.nodes[node]['status'] = 'influenced'
             return True
         
         # neither R nor T can active this node
@@ -117,6 +135,7 @@ class LTD1DT_model:
             G = specified_G.G
         else:
             G = self.GWA.G
+            specified_G = self.GWA
         
         if (G.nodes[node]['status'] == 'R-active') or (G.nodes[node]['status'] == 'T-active'):
             return False
@@ -134,17 +153,17 @@ class LTD1DT_model:
             return False
 
         if (R_active_num/active_num) > G.nodes[node]['d_threshold']:
-            specified_G.get_final_R_receiver()[node]='R'
+            specified_G.final_R_receiver[node]='R'
             G.nodes[node]['status']='R-active'
             if node in influence_nodes.keys():
                 influence_nodes.pop(node)
             return True
 
         elif T_active_num > 0 :
-            specified_G.get_final_T_receiver()[node]='T'
+            specified_G.final_T_receiver[node]='T'
             G.nodes[node]['status']='T-active'
-            if node in self.final_R_receiver:
-                specified_G.get_final_R_receiver().pop(node)
+            if node in specified_G.final_R_receiver:
+                specified_G.final_R_receiver.pop(node)
             if node in influence_nodes.keys():
                 influence_nodes.pop(node)
             return True
@@ -152,10 +171,171 @@ class LTD1DT_model:
         return False
     
     
-    def get_diffused_G(self):
-        return nx.Graph(self.G)
-    def get_init_G(self):
-        return nx.Graph(self.init_G)
+    def update_seed_T(self,new_seed_T:list,cover:bool=True,specified_G:Graph_with_attr=None):
+        '''Update specifiy GWA(Graph with attribution)'s seed_T
+        
+        Parameters
+        -----------
+        new_seed_T: list
+            list of nodes
+
+        cover: bool 
+            Default True. If cover is True, then new seed_T will 
+            cover the specified G's seed_T. Else new seed_T will append to original seed_T
+
+        specified_G: Graph_with_attr
+            Default None. If a GWA was specified, then will change the GWA's attrbution.
+            Else the attribution of model's GWA will be changed.
+
+        '''
+        if cover:
+            self.seed_T = new_seed_T.copy()
+        else:
+            self.seed_T+=new_seed_T
+
+        if specified_G is not None:
+            G = specified_G.G
+        else:
+            G = self.GWA.G
+            specified_G = self.GWA
+
+        for node in self.seed_T:
+            G.nodes[node]['status']='T-active'
+            specified_G.final_T_receiver[node]='T'
 # %%
+class model_V2:
+    def __init__(self,G:nx.Graph,is_init:bool=False,seed_T:list=None,seed_R:list=None) -> None:
+        '''Creating a LTD1TD model that running on G
+
+        Parameters
+        -----------
+        G : nx.Graph
+            A networkX's graph.
+        is_init : bool
+            Default False. If True, the model will use the original attributions of G.
+        seed_T : list
+            A list of T nodes.
+        seed_R : list
+            A list of R nodes.
+        '''
+        
+        self.G = nx.Graph(G)
+        self.final_R_receiver = {}
+        self.final_T_receiver = {}
+
+        if seed_T is not None:
+            self.seed_T = seed_T.copy()
+        else:
+            self.seed_T = []
+        if seed_R is not None:
+            self.seed_R = seed_R.copy()
+        else:
+            self.seed_R = []
+
+        # init Graph's threshold & status
+        if not is_init:
+            for node in nx.nodes(G):
+                G.nodes[node]['i_threshold']=np.random.uniform()
+                G.nodes[node]['d_threshold']=np.random.uniform()
+                G.nodes[node]['status']='inactive'
+        # init R-seed nodes
+        if len(seed_R) !=0:
+            for node in seed_R:
+                G.nodes[node]['status']='R-active'
+                self.final_R_receiver[node]='R'
+        # init T-seed nodes
+        if len(seed_T) != 0:
+            for node in seed_T:
+                G.nodes[node]['status']='T-active'
+                self.final_T_receiver[node]='T'
+
+
+    
+    def diffusion(self,is_apply:bool=False):
+
+        if not is_apply:
+            G = nx.Graph(self.G)
+            final_T_receiver = self.final_T_receiver.copy()
+            final_R_receiver = self.final_R_receiver.copy()
+        else:
+            G = self.G
+            final_R_receiver = self.final_R_receiver
+            final_T_receiver = self.final_T_receiver
+        
+        nothing_change = False
+
+        while(not nothing_change):
+
+            nothing_change = True
+
+            # influence stage
+            for node in nx.nodes(G):
+                if G.nodes[node]['status'] == 'inactive':
+                    if self.__check_i_threshold(node,G):
+                        nothing_change = False
+            
+            # decision stage
+            for node in nx.nodes(G):
+                if node not in self.seed_R:
+                    if self.__check_d_threshold(node,G):
+                        nothing_change = False
+
+    
+    def __check_i_threshold(self,node,G:nx.Graph):
+        influenced_num = 0
+        node_deg = nx.degree(G,node)
+        if node_deg == 0:
+            return False
+
+        for nbr in nx.neighbors(G,node):
+            if (G.nodes[nbr]['status'] == 'R-active') or (G.nodes[nbr]['status'] == 'T-active') :
+                influenced_num+=1
+        
+        if (influenced_num/node_deg) >= G.nodes[node]['i_threshold']:
+            G.nodes[node]['status'] = 'influenced'
+            return True
+        
+        return False
+    
+    def __check_d_threshold(self,node,G:nx.Graph,):
+        active_num = 0
+        R_active_num = 0
+        T_active_num = 0
+
+        if (G.nodes[node]['status'] == 'T-active'):
+            return False
+        
+        for nbr in nx.neighbors(G,node):
+            if G.nodes[nbr]['status'] == 'R-active':
+                active_num+=1
+                R_active_num+=1
+            elif G.nodes[nbr]['status'] == 'T-active':
+                active_num+=1
+                T_active_num+=1
+        
+        if active_num == 0:
+            return False
+
+        if (R_active_num/active_num) > G.nodes[node]['d_threshold']:
+            G.final_R_receiver[node]='R'
+            G.nodes[node]['status']='R-active'
+            return True
+
+        elif T_active_num > 0 :
+            G.final_T_receiver[node]='T'
+            G.nodes[node]['status']='T-active'
+            if node in G.final_R_receiver:
+                G.final_R_receiver.pop(node)
+            return True
+        
+        return False
+
+
+    def get_initialized_G(self,):
+        return nx.Graph(self.G)
+
+    def copy(self,):
+        G = nx.Graph(self.G)
+        return self.__class__(G,True,self.seed_T.copy(),self.seed_R.copy())
 # %%
 # %%
--- study_experiment/Containment of rumor spread in complex social networks/algorithm.py
+++ study_experiment/Containment of rumor spread in complex social networks/algorithm.py
@@ -9,28 +9,34 @@ class unconstrained_algorithm:
         #self.init_model  = diffusion_model.copy()
         pass
 
-    def MinGreedy(self,GWA:LTD1DT.Graph_with_attr,k:int,seed_R:list):
+    def MinGreedy(self,model:LTD1DT.LTD1DT_model,GWA:LTD1DT.Graph_with_attr,k:int,seed_R:list):
         seed_T = []
-        model_1 = LTD1DT.LTD1DT_model(GWA,seed_R,seed_T)
-        model_1.diffusion_simulation()# GWA.G didn't change
-        final_R_num = len(model_1.GWA.final_R_receiver())
+        initialized_GWA = GWA.copy()
+
+        temp_model = model.copy()
+        temp_model.diffusion_simulation(initialized_GWA,)
+        final_R_num = len(initialized_GWA.final_R_receiver)
         
         
         while(len(seed_T)<k):
             choosen_node = None
-            temp_GWA = GWA.copy()
+            temp_GWA = initialized_GWA.copy()
+
             # find nodes which minimize the spread of rumor
             for node in temp_GWA.G.nodes():
-
+                
                 if (node not in seed_R) and (node not in seed_T):
-                    model_1.diffusion_simulation(temp_GWA)
+                    model.update_seed_T([node],temp_GWA)
+                    model.diffusion_simulation(temp_GWA) 
                     if final_R_num > len(temp_GWA.get_final_R_receiver()):
                         final_R_num = len(temp_GWA.get_final_R_receiver())
                         choosen_node = node
+                    else:
+                        temp_GWA = initialized_GWA.copy()
             
             if choosen_node is not None:
                 seed_T.append(choosen_node)
-                model_1.seed_T.append(choosen_node)
+                model.update_seed_T(seed_T,initialized_GWA)
             else:
                 print(f'can not find k nodes of T_seed, only {len(seed_T)} nodes found.')
                 return seed_T
@@ -39,13 +45,21 @@ class unconstrained_algorithm:
 # %%
 
 '''------------------ test below -----------------------'''
-G = nx.path_graph(range(1,15))
+G = nx.Graph()
+G.add_edges_from([(1,2),(1,7),(2,3),(3,4),(3,5),(4,6),(7,6),(7,8),(8,9),(9,5)])
+nx.draw_networkx(G,pos=nx.circular_layout(G),with_labels=True,font_color='white')
+#%%
 GWA = LTD1DT.Graph_with_attr(G)
-model = LTD1DT.LTD1DT_model(GWA,[6,8],[])
+model = LTD1DT.LTD1DT_model(GWA,[],[])
+initialized_GWA = model.GWA.copy()
 # %%
-nx.get_node_attributes(GWA.G,'status')
+nx.get_node_attributes(model.GWA.G,'i_threshold')
 # %%
-model.diffusion_simulation(GWA)
+nx.get_node_attributes(initialized_GWA.G,'status')
 # %%
-
+# %%
+un_al = unconstrained_algorithm()
+un_al.MinGreedy(model,initialized_GWA,3,[6,8,])
+# %%
+nx.get_node_attributes(initialized_GWA.G,'status')
 # %%
